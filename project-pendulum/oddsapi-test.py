a4eea584c0640634f9c9ec0c9cd41dc3f3613967
chipaces <dcamdenjones@gmail.com>
2023-12-11 23:43:42 -0500

    deleted duplicate script

--- project-pendulum/oddsapi-test.py
+++ project-pendulum/oddsapi-test.py
@@ -1,11 +1,13 @@
 import requests
+import sys
 
 api_key='632f34156395fe9b6d29b1fdca78ad52'
 url='https://api.the-odds-api.com/v4/sports/?apiKey='
 
 response=requests.get(url+api_key)
-if response.status_code == 200:
-    data=response.json()
-else:
+if response.status_code != 200:
     print(f"Failed to fetch data: {response.status_code}")
+    sys.exit(1)
+data=response.json()
+
 print(data[0])
--- project-pendulum/streaks.py
+++ project-pendulum/streaks.py
@@ -1,41 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import lxml
+import sys
 
 # Specify URL to teams table and request permission from servers
 
 nba_url = "https://www.oddsshark.com/nba/ats-standings"
-#nhl_url = 
 cbb_url = "https://www.oddsshark.com/ncaab/ats-standings"
+# TODO: look for nhl_url (not on OddShark)
 page = requests.get(cbb_url)
-#print(page.status_code)
+if page.status_code != 200:
+    print(f"Failed to fetch data: {page.status_code}")
+    sys.exit(1)
 
 # pass page object into BS
 soup = BeautifulSoup(page.content, 'lxml')
-#print(soup.prettify())
 
-# Build list of conf tables (OddShark groups by conf)
+# tables is list of HTML table elements (NBA divs, NCAAB confs)
 tables = soup.find_all('table', class_='table--striped table--fixed-column caption--plain table block block-oddsshark-data-blocks block-standings-block')
-#print(len(tables)) # number of CBB D1 conferences
 
 headers = []
-# Use tables[0] to get col names (tables[0]==America East)
+# Use tables[0] to get col names
 for col in tables[0].find_all('th'):
-    title = col.text.strip()
+    title = col.text.strip().replace(' ','-')
     headers.append(title)
-#print (headers)
 
-# build dataframe
 df = pd.DataFrame(columns=headers)
 
 # combine all tables into df
 # each <tr class> is a row, with several <td class> col values
-for conf in tables:
-    for j in conf.find_all('tr')[1:]:
-        row_data = j.find_all('td')
-        row = [i.text for i in row_data]
+for t in tables:
+    for i in t.find_all('tr')[1:]:
+        row_data = i.find_all('td')
+        row = [j.text for j in row_data]
         df.loc[len(df)] = row
 
-df1 = df.sort_values('ATS Streak',ascending=False).head(15)
-print(df1)
+df.sort_values('ATS-Streak',ascending=False,inplace=True)
+print(df[['Team','ATS-Streak']].head(15))
