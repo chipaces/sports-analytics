ddfa9d860765ce3c51073182898a8ea851e24111
chipaces <dcamdenjones@gmail.com>
2024-01-05 00:32:36 -0500

    clean up, new TR script

--- project-pendulum/streaks.py
+++ project-pendulum/streaks.py
@@ -3,37 +3,59 @@ import requests
 import pandas as pd
 import sys
 
-# Specify URL to teams table and request permission from servers
+def main(sport):
+    # Specify URL to teams table and request permission from servers
+    urls = {
+        "nba": "https://www.oddsshark.com/nba/ats-standings",
+        "cbb": "https://www.oddsshark.com/ncaab/ats-standings"
+        # TODO: look for nhl_url (not on OddShark)
+    }
+    if sport not in urls.keys():
+        print("Invalid arg!")
+        sys.exit(1)
+    
+    page = requests.get(urls[sport])
+    if page.status_code != 200:
+        print(f"Failed to fetch data: {page.status_code}")
+        sys.exit(1)
 
-nba_url = "https://www.oddsshark.com/nba/ats-standings"
-cbb_url = "https://www.oddsshark.com/ncaab/ats-standings"
-# TODO: look for nhl_url (not on OddShark)
-page = requests.get(cbb_url)
-if page.status_code != 200:
-    print(f"Failed to fetch data: {page.status_code}")
-    sys.exit(1)
+    # pass page object into BS
+    soup = BeautifulSoup(page.content, 'lxml')
 
-# pass page object into BS
-soup = BeautifulSoup(page.content, 'lxml')
+    # tables is list of HTML table elements (NBA divs, NCAAB confs)
+    tables = soup.find_all('table', class_='table--striped table--fixed-column caption--plain table block block-oddsshark-data-blocks block-standings-block')
 
-# tables is list of HTML table elements (NBA divs, NCAAB confs)
-tables = soup.find_all('table', class_='table--striped table--fixed-column caption--plain table block block-oddsshark-data-blocks block-standings-block')
+    headers = []
+    # Use tables[0] to get col names
+    for col in tables[0].find_all('th'):
+        title = col.text.strip().replace(' ','-')
+        headers.append(title)
 
-headers = []
-# Use tables[0] to get col names
-for col in tables[0].find_all('th'):
-    title = col.text.strip().replace(' ','-')
-    headers.append(title)
+    df = pd.DataFrame(columns=headers)
 
-df = pd.DataFrame(columns=headers)
+    # combine all tables into df
+    # each <tr class> is a row, with several <td class> col values
+    for t in tables:
+        for i in t.find_all('tr')[1:]:
+            row_data = i.find_all('td')
+            row = [j.text for j in row_data]
+            df.loc[len(df)] = row
 
-# combine all tables into df
-# each <tr class> is a row, with several <td class> col values
-for t in tables:
-    for i in t.find_all('tr')[1:]:
-        row_data = i.find_all('td')
-        row = [j.text for j in row_data]
-        df.loc[len(df)] = row
+    df['Streak-Length'] = df['ATS-Streak'].str.extract('(\d+)').astype(int)
+    df.sort_values('Streak-Length',ascending=False,inplace=True)
 
-df.sort_values('ATS-Streak',ascending=False,inplace=True)
-print(df[['Team','ATS-Streak']].head(15))
+    # TODO: add parameter for 'streak_length', 'cumul'
+        # streak_len (int, Default=6): int value corres
+        # cumul (bool, Default=True): true->shows all streaks >= streak_len. false->shows only streaks==streak_len
+    # TODO: add feature for no sport given -> show both/all sport tables side-by-side
+        # (combine dataframes of both/all sports)
+    # Can't help but feel like I'm using a Python script to do a database's job
+
+    print(df[df['Streak-Length'] >= 6][['Team', 'ATS-Streak']])
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing arguments!")
+        sys.exit(1)
+    main(sys.argv[1].lower())
+        
---
+++ project-pendulum/streaks2.py
@@ -0,0 +1,76 @@
+# Remix of streaks.py to pull from TeamRankings
+# From teamrankings.com/ncb/trends/ats_trends :
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sys
+import datetime as dt
+
+# team rankings home page
+tr_url = "https://www.teamrankings.com/ncb/trends/ats_trends/"
+# Define lists for streak frequencies
+ats_freqs=[0]*20
+ovr_freqs=[0]*20
+undr_freqs=[0]*20
+atsStrk=0
+ovrStrk=0
+date = dt.date(2023,1,4)
+
+page = requests.get(tr_url)
+if page.status_code != 200:
+    print(f"Failed to fetch data: {page.status_code}")
+    sys.exit(1)
+
+
+
+# use SoupStrainer to just grab the teams table?
+# Get full page from TeamRankings
+soup = BeautifulSoup(page.content, 'lxml')
+
+# find_all returns set of PageElements ... this is (I assume) of size 1
+teams_table = soup.find_all('table',id_='DataTables_Table_0')
+print(type(teams_table))
+
+links = list()
+for link in teams_table.find_all('a'):
+    links.append(link.get('href'))
+    #print(link.get('href'))
+print(len(links))
+# Question: faster to import all team schedules as DFs (and flagging bold values somehow) before 
+    # doing streak analysis, or analyze as we import?
+
+# For each D1 team:
+    # Grab its team page (w BeautifulSoup?)
+    # Grab its Betting View table (HTML element)
+    # For each <tr> in table <tbody> (each <tr> is a game), or ... while <tr>[0].date < TODAY (?):
+        # look at the 7,8,9th (1-indexed) <td> elements (<td class="betting text-right" ... /td>)
+        # if 7th <td> elem is bold (style="font-weight: bold;"):
+            # if atsStrk < 0:
+                # ats_freqs[abs(atsStrk)] += 1
+                # atsStrk = 1
+            # Else incremement ATS Streak
+        # else (if team did not cover):
+            # check if team was on ATS win streak
+            # if atsStrk > 0:
+                # ats_freqs[abs(atsStrk)] += 1
+                # set atsStreak = -1
+            # else decrement ATS_Streak
+
+        # if 8th <td> elem is bold (style="font-weight: bold;"):
+            # check if team was on under streak
+            # if ovrStrk < 0:
+                # under_freqs[abs(ovrStrk)] += 1
+                # set ovrStrk = 1
+            # else increment ovrStrk
+        # else:
+            # check if team was on over streak
+            # if ovrStrk > 0:
+                # ovr_freqs[ovrStrk] += 1
+                # set OvrStrk = -1
+            # else decrement ovrStrk
+
+
+
+
+# dataframe headers : Date,Opponent,Result,Spread,Total,Money
+# <table class="tr-table datatable scrollable dataTable no-footer" id="DataTables_Table_0" role="grid">
